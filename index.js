30740282a00d4196f24536a88cbfe11f7b532441
孔祥海 <kongxianghai@shgbit.com>
2016-10-19 13:02:11 +0800

    add read file line

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 "use strict"
+var fs = require('fs');
+var readline = require('readline');
 
 var csv = {};
 module.exports = csv;
@@ -7,13 +9,32 @@ csv.out = function (data) {
 
 };
 
-csv.outEachLine = function(data,callback) {
+csv.outEachLine = function (data, callback) {
 
 };
 
-csv.outAll = function(data,callback){
+csv.outAll = function (data, callback) {
 
 }
 
+var readFileLine = (path) => {
+    var inEnd = true;
+    var index = 0;
 
+    var fReadStream = fs.createReadStream(path,{encoding:'utf8'});
 
+    var objReadline = readline.createInterface({
+        input: fReadStream
+    });
+
+    fReadStream.on('end', function () {
+        console.log('end');
+        inEnd = true;
+    });
+
+    objReadline.on('line', function (line) {
+        console.log(index++, line);
+    });
+}
+
+readFileLine('test/demo2.csv');
